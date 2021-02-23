0ee317891be3fdff023e44cda305b13e96eca59b
ArturasVan <arturas.vaninas@gmail.com>
2021-02-23 12:17:51 +0100

    last

---
+++ Controllers/ImageController.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BookStore.Data;
+using BookStore.Models;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+
+namespace BookStore.Controllers
+{
+    public class ImageController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IWebHostEnvironment _hostEnvironment;
+
+        public ImageController(ApplicationDbContext context, IWebHostEnvironment hostEnvironment)
+        {
+            _context = context;
+            _hostEnvironment = hostEnvironment;
+        }
+
+        // GET: Image
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.ProductPictures.Include(p => p.Product);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Image/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var productPicture = await _context.ProductPictures
+                .Include(p => p.Product)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (productPicture == null)
+            {
+                return NotFound();
+            }
+
+            return View(productPicture);
+        }
+
+        // GET: Image/Create
+        public IActionResult Create()
+        {
+            ViewData["ProductId"] = new SelectList(_context.Product, "ProductId", "ProductId");
+            return View();
+        }
+
+        // POST: Image/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,ProductId,Url,ImageFile")] ProductPicture productPicture)
+        {
+            if (ModelState.IsValid)
+            {
+                //Save image to wwwroot/image
+                string wwwRootPath = _hostEnvironment.WebRootPath;
+                string fileName = Path.GetFileNameWithoutExtension(productPicture.ImageFile.FileName);
+                string extension = Path.GetExtension(productPicture.ImageFile.FileName);
+                productPicture.Url = fileName = fileName + DateTime.Now.ToString("yymmssfff") + extension;
+                string path = Path.Combine(wwwRootPath + "/Image/", fileName);
+                using (var fileStream = new FileStream(path, FileMode.Create))
+                {
+                    await productPicture.ImageFile.CopyToAsync(fileStream);
+                }
+                _context.Add(productPicture);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ProductId"] = new SelectList(_context.Product, "ProductId", "ProductId", productPicture.ProductId);
+            return View(productPicture);
+        }
+
+        // GET: Image/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var productPicture = await _context.ProductPictures.FindAsync(id);
+            if (productPicture == null)
+            {
+                return NotFound();
+            }
+            ViewData["ProductId"] = new SelectList(_context.Product, "ProductId", "ProductId", productPicture.ProductId);
+            return View(productPicture);
+        }
+
+        // POST: Image/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,ProductId,Url")] ProductPicture productPicture)
+        {
+            if (id != productPicture.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(productPicture);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProductPictureExists(productPicture.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ProductId"] = new SelectList(_context.Product, "ProductId", "ProductId", productPicture.ProductId);
+            return View(productPicture);
+        }
+
+        // GET: Image/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var productPicture = await _context.ProductPictures
+                .Include(p => p.Product)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (productPicture == null)
+            {
+                return NotFound();
+            }
+
+            return View(productPicture);
+        }
+
+        // POST: Image/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var productPicture = await _context.ProductPictures.FindAsync(id);
+            _context.ProductPictures.Remove(productPicture);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ProductPictureExists(int id)
+        {
+            return _context.ProductPictures.Any(e => e.Id == id);
+        }
+    }
+}
